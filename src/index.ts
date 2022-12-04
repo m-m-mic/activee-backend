0f91e10de8cc9cc986281d23fc9dfc8f880f8d5b
Michael Buchholz <michael.j.buchholz@t-online.de>
2022-12-04 18:26:00 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
+import cors from "cors";
 import express from "express";
-import { helloWorld } from "./hello";
 import { addActivity, getActivities, getActivityById, updateActivityById } from "./initialActivity";
 const app = express();
 const port = 1337;
 
-helloWorld();
+app.use(cors());
 
 app.use(express.json());
 app.get("/", (req, res) => {
