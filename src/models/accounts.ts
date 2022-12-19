996d972fa8b0a56712bbe0296acb181b5f125eeb
Michael Buchholz <michael.j.buchholz@t-online.de>
2022-12-19 12:18:18 +0100

    fixed unique password error for children accounts

--- src/models/accounts.ts
+++ src/models/accounts.ts
@@ -3,23 +3,23 @@ import mongoose from "mongoose";
 // Mongoose Schema für einen Account
 const AccountSchema = new mongoose.Schema({
   email: { type: String, trim: true, unique: true, sparse: true },
-  password: String,
+  password: { type: String, trim: true },
   type: { type: String, required: true },
   tier: {
     type: String,
     required: true,
   },
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
+  first_name: { type: String, trim: true, required: true },
+  last_name: { type: String, trim: true, required: true },
   club: String,
   phone_number: String,
   birthday: String,
   address: {
     type: {
-      street: String,
-      house_number: String,
-      zip_code: String,
-      city: String,
+      street: { type: String, trim: true },
+      house_number: { type: String, trim: true },
+      zip_code: { type: String, trim: true },
+      city: { type: String, trim: true },
     },
     _id: false,
   },
