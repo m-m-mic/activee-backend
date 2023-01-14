dc98511a46958e5a466b86068f57d530418df3fd
hm-madl <madl@hm.edu>
2023-01-14 13:47:59 +0100

    added individual sport route

--- src/models/sports.ts
+++ src/models/sports.ts
@@ -4,6 +4,11 @@ import mongoose from "mongoose";
 const SportSchema = new mongoose.Schema({
   _id: { type: String, required: true, trim: true },
   name: { type: String, required: true, trim: true },
+  description: {
+    instruction: { type: String, default: "no instruction given", trim: true },
+    history: { type: String, default: "no history given", trim: true },
+  },
+  activities: [{ type: mongoose.Schema.Types.ObjectId, ref: "Activity" }],
 });
 
 export const Sport = mongoose.model("Sport", SportSchema);
--- src/routes/sportRoutes.ts
+++ src/routes/sportRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Sport } from "../models/sports";
+import { authenticateJWT } from "../middleware/authenticateJWT";
 
 export const sportRoutes = express.Router();
 
@@ -17,13 +18,33 @@ sportRoutes.post("/sport/", async (req, res) => {
 });
 
 // GET-Request zum Aufrufen aller verfügbaren Sportarten
-sportRoutes.get("/sport/", async (req, res) => {
+sportRoutes.get("/sport/", authenticateJWT, async (req, res) => {
   try {
-    const sports = await Sport.find();
+    const sports = await Sport.find().sort({ name: 1 });
     return res.send(sports);
   } catch (error) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     return res.status(400).send(error.message);
   }
+});
+
+sportRoutes.get("/sport/:sportId", authenticateJWT, async (req, res) => {
+  const id = req.params.sportId;
+  try {
+    const sport = await Sport.findOne({ _id: id });
+    if (!sport) {
+      return res.status(404).send("Sport not found");
+    }
+    return res.send(sport);
+  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return res.status(400).send(error.message);
+  }
+});
+
+sportRoutes.patch("/sport/dev", async (req, res) => {
+  await Sport.updateMany({}, { description: { instruction: "no instruction given", history: "no history given" } });
+  res.send("lol");
 });
